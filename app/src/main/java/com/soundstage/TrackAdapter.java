5ca8bfc97d8a645dd0fc12bf600aca45d615165e
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-17 16:35:38 -0600

    Create TrackAdapter.java

---
+++ app/src/main/java/com/soundstage/TrackAdapter.java
@@ -0,0 +1,41 @@
+import android.view.*;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class TrackAdapter extends RecyclerView.Adapter<TrackAdapter.ViewHolder> {
+
+    List<String> tracks;
+
+    public TrackAdapter(List<String> tracks) {
+        this.tracks = tracks;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_track, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(tracks.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return tracks.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(android.R.id.text1);
+        }
+    }
+}
