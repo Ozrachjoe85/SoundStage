e9078dea59e9e474e72de9a3353291339083a647
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-17 16:50:11 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,24 @@
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+// build.gradle (project root)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath "com.android.tools.build:gradle:9.4.0"
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20"
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// Prevent deprecated or dynamic dependency issues
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
 }
