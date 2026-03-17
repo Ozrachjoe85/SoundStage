fe2e0c0714e55b30fdb1f441790ec187bb76360f
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-17 15:31:31 -0600

    Update EqualizerScreen.kt

--- app/src/main/java/com/soundstage/app/ui/EqualizerScreen.kt
+++ app/src/main/java/com/soundstage/app/ui/EqualizerScreen.kt
@@ -1,1 +1,11 @@
+package com.soundstage.app.ui
 
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+
+@Composable
+fun EqualizerScreen() {
+    Column {
+        Text("Equalizer (Auto + Manual)")
+    }
+}
