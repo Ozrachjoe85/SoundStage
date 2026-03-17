8d95412b9f9975378a464a11aaed4d25b1052297
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-17 15:30:31 -0600

    Update VisualizerEngine.kt

--- app/src/main/java/com/soundstage/app/visualizer/VisualizerEngine.kt
+++ app/src/main/java/com/soundstage/app/visualizer/VisualizerEngine.kt
@@ -1,1 +1,24 @@
+package com.soundstage.app.visualizer
 
+import android.media.audiofx.Visualizer
+
+class VisualizerEngine(sessionId: Int) {
+
+    private val visualizer = Visualizer(sessionId)
+
+    fun start(onData: (ByteArray) -> Unit) {
+        visualizer.setDataCaptureListener(
+            object : Visualizer.OnDataCaptureListener {
+                override fun onWaveFormDataCapture(v: Visualizer?, data: ByteArray?, rate: Int) {
+                    data?.let { onData(it) }
+                }
+
+                override fun onFftDataCapture(v: Visualizer?, data: ByteArray?, rate: Int) {}
+            },
+            Visualizer.getMaxCaptureRate() / 2,
+            true,
+            false
+        )
+        visualizer.enabled = true
+    }
+}
