821c589076b35089b6b169cc219db0e308fe0de1
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-18 11:48:29 -0600

    Create CoreAudioState.kt

---
+++ app/src/main/java/com/soundstage/app/audio/CoreAudioState.kt
@@ -0,0 +1,8 @@
+package com.soundstage.app.audio
+
+data class CoreAudioState(
+    val bass: Float = 0.5f,
+    val mid: Float = 0.5f,
+    val treble: Float = 0.5f,
+    val width: Float = 0.5f
+)
